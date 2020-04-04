09b89f3548814a2c9ade99a58dfbae02b09af72b
Bruno Kawka <brunokawka@gmail.com>
2020-04-04 19:04:15 +0200

    Add files via upload

---
+++ bash/template.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#pragma GCC target("popcnt")
+#define fastIO      \
+  std::cin.tie(0);  \
+  std::cout.tie(0); \
+  std::ios::sync_with_stdio(0);
+#define endl "\n"
+template <typename T>
+using v = std::vector<T>;
+template <typename T>
+using q = std::queue<T>;
+template <typename T>
+using pq = std::priority_queue<T>;
+template <typename T>
+using set = std::set<T>;
+template <typename T>
+using uset = std::unordered_set<T>;
+template <typename K, typename V>
+using umap = std::unordered_map<K, V>;
+template <typename K, typename V>
+using map = std::map<K, V>;
+typedef long long int i64;
+typedef unsigned long long ui64;
+typedef std::string str;
+typedef std::vector<int> vint;
+typedef std::vector<char> vchar;
+typedef std::vector<std::string> vstr;
+typedef std::pair<int, int> pint;
+
+
+
+int main() {
+  fastIO;
+
+  
+
+  return 0;
+}
+
+/** ℹ Useful
+ * __builtin_popcount/ll
+ * __builtin_clz/ll
+ * __builtin_ctz/ll
+ */
+
+/** ⚠ Common mistakes
+ *
+ */
